ed296804f56314652441a0965c11445950f67622
ankur.sharma <ankur.sharma@lns.maersk.com>
2025-07-31 11:07:48 +0200

    First iteration

---
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,9 @@
+package stringcalculator;
+
+public class StringCalculator {
+
+
+    public int addNumbers(String inputString) {
+        return 0;
+    }
+}
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -2,11 +2,24 @@ package stringcalculator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringCalculatorTest {
+
+    StringCalculator stringCalculator;
+
     @Test
     void isTrue() {
         assertTrue(true);
     }
+
+    @Test
+    void testMethod(){
+        stringCalculator = new StringCalculator();
+        String inputString = "1,2";
+        int sum = stringCalculator.addNumbers(inputString);
+        assertEquals(3, sum);
+    }
+
 }
