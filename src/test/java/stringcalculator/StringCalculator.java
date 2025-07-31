1dc15c620f8dd58236a578dcc1131280467c19c6
Christian Anker Larsen <christian.anker.larsen@lns.maersk.com>
2025-07-31 12:58:37 +0200

    delete unused method

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -32,32 +32,4 @@ public class StringCalculator {
         return 3;
     }
 
-    public int addNumbers(String inputString) {
-        if(inputString.isEmpty()) {
-            return 0;
-        }
-        if(inputString.length() == 1){
-            return Integer.parseInt(inputString);
-        }
-        String[] strArr  = inputString.split(",");
-        int[] intArr = new int[strArr.length];
-
-        for (int i = 0; i < strArr.length; i++) {
-            intArr[i] = Integer.parseInt(strArr[i]);
-        }
-
-        int sum = 0;
-
-        for (int i = 0; i < intArr.length; i++) {
-            sum = sum + intArr[i];
-        }
-
-        if(inputString.equals("4,5")) {
-            return 9;
-        }
-        if(inputString.equals("5,6")) {
-            return 11;
-        }
-        return 3;
-    }
 }
