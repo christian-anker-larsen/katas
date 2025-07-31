b1b2cba54c1f1623234325d67eed6e421573ee77
Christian Anker Larsen <christian.anker.larsen@lns.maersk.com>
2025-07-31 12:53:46 +0200

    remove duplicate tests

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -17,27 +17,9 @@ class StringCalculatorTest {
     }
 
     @Test
-    void testMethod() {
-        int sum = stringCalculator.addNumbers("1,2");
-        assertEquals(3, sum);
-    }
-
-    @Test
     void isEmpty() {
         int sum = stringCalculator.addNumbers("");
         assertEquals(0, sum);
-    }
-
-    @Test
-    void testAdd2Numbers2() {
-        int sum = stringCalculator.addNumbers("4,5");
-        assertEquals(9, sum);
-    }
-
-    @Test
-    void testAdd2Numbers3() {
-        int sum = stringCalculator.addNumbers("5,6");
-        assertEquals(11, sum);
     }
 
     @Test
