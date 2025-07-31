c2dc5bd6f79741e431d3eaeffe7520e1f3e27ea0
jithesh-a <jithesh.a@lns.maersk.com>
2025-07-31 14:51:59 +0530

    wip

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -4,6 +4,9 @@ public class StringCalculator {
 
 
     public int addNumbers(String inputString) {
+        if(inputString.isEmpty()) {
+            return 0;
+        }
         return 3;
     }
 }
