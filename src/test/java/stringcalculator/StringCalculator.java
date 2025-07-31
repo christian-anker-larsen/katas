cf76e8fdffdfbe45082bf733e191f8178c5c994c
Christian Anker Larsen <christian.anker.larsen@lns.maersk.com>
2025-07-31 12:58:53 +0200

    make package private

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -3,7 +3,7 @@ package stringcalculator;
 public class StringCalculator {
 
 
-    public static int add(String numbers) {
+    static int add(String numbers) {
         if(numbers.isEmpty()) {
             return 0;
         }
