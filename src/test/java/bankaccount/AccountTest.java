eaacfcd2700b15f4e2f4fcbdf56f431b659e2bde
monika-dalal <monika.dalal@maersk.com>
2025-09-11 15:54:39 +0530

    Added test case

--- src/test/java/bankaccount/AccountTest.java
+++ src/test/java/bankaccount/AccountTest.java
@@ -1,8 +1,18 @@
 package bankaccount;
 
 import net.gamrath.junitpredict.Predict;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(Predict.class)
 class AccountTest {
+
+    @Test
+    void withdraw100() {
+        Account account;
+        account.withdraw(100);
+        assertEquals(50,account.balance());
+    }
 }
