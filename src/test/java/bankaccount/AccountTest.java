9e8daf2391de0d6aa0e5e3fdf849a226f1d385dc
Christian Anker Larsen <christian.anker.larsen@lns.maersk.com>
2025-09-11 12:42:07 +0200

    wip

--- src/test/java/bankaccount/AccountTest.java
+++ src/test/java/bankaccount/AccountTest.java
@@ -1,6 +1,7 @@
 package bankaccount;
 
 import net.gamrath.junitpredict.Predict;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -9,27 +10,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(Predict.class)
 class AccountTest {
 
+    private Account account;
+
+    @BeforeEach
+    void setUp() {
+        account = new Account();
+        account.setBalance(100);
+    }
+
     @Test
     void withdraw100() {
-        Account account = new Account();
-        account.setBalance(100);
         account.withdraw(50);
         assertEquals(50,account.balance);
     }
 
     @Test
     void deposit100() {
-        Account account = new Account();
-        account.setBalance(100);
         account.deposit(100);
         assertEquals(200,account.getBalance());
     }
 
     @Test
     void deposit200() {
-        Account account = new Account();
-        account.setBalance(100);
         account.deposit(200);
-        assertEquals(300,account.getBalance());
+        assertEquals(300, account.getBalance());
     }
 }
