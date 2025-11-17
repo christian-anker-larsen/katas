cfa11cc1aed773a5692d3ffd06640b64fb24e773
Frederik Alexander Vygg Larsen <frederik.alexander.larsen@lns.maersk.com>
2025-11-17 12:42:21 +0100

    wip

--- src/test/java/bankaccount/AccountTest.java
+++ src/test/java/bankaccount/AccountTest.java
@@ -3,12 +3,14 @@ package bankaccount;
 import Account.Account;
 import org.junit.jupiter.api.Test;
 
-public class AccountTest {
+class AccountTest {
 
     @Test
     void testDeposits() {
         // Test code for deposits
         Account account = new Account();
         account.deposit(100);
+
+//        int balance = account.getBalance();
     }
 }
